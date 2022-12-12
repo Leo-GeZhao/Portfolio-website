acfccd3d1cb086167ce4bd824bcebc4602afd5bc
Ge Zhao <gezhao@Ges-MacBook-Pro.local>
2022-12-12 03:25:34 -0500

    fix nav__logo

--- src/sections/navbar/Navbar.jsx
+++ src/sections/navbar/Navbar.jsx
@@ -1,10 +1,11 @@
+/* eslint-disable jsx-a11y/anchor-is-valid */
 import './navbar.css'
 import data from './data'
 const Navbar = () => {
   return (
     <nav>
       <div className='container nav__container'>
-        <a href="index.html"><span className='nav__logo'>Leo Zhao</span></a>
+        <a href="#"><span className='nav__logo'>Leo Zhao</span></a>
         <ul className='nav__menu'>
           {
             data.map((item,index)=><li key={index+1}><a href={item.link}>{item.title}</a></li>)
