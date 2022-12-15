14384bbe5f6e7fd56dca620ac3cf0f665b1c98f7
Ge Zhao <gezhao@Ges-MacBook-Pro.local>
2022-12-15 03:23:23 -0500

    add projects image

--- src/sections/portfolio/data.js
+++ src/sections/portfolio/data.js
@@ -1,9 +1,11 @@
-import headerImg from '../../assets/HeaderImg.jpg'
+import HustleTo from '../../assets/projects/Hustle-To.png'
+import Nomadic from '../../assets/projects/Nomadic.png'
+import Tracker from '../../assets/projects/Tracker.png'
 
 const data = [
-    {img:headerImg, title:"HustleTo", desc:"Ecommerce website using Mern-Stack", demo:"https://hustle-to.up.railway.app/",github:"https://github.com/Leo-GeZhao/Hustle-To"},
-    {img:headerImg, title:"Nomadic", desc:"Group project using Django", demo:"https://nomadic-app.herokuapp.com/",github:"https://github.com/Leo-GeZhao/nomadic"},  
-    {img:headerImg, title:"Tracker", desc:"personal project using Django", demo:"https://tracker.herokuapp.com/",github:"hhttps://github.com/Leo-GeZhao/Tracker"}
+    {img:HustleTo, title:"HustleTo", desc:"Ecommerce website using Mern-Stack", demo:"https://hustle-to.up.railway.app/",github:"https://github.com/Leo-GeZhao/Hustle-To"},
+    {img:Nomadic, title:"Nomadic", desc:"Group project using Django", demo:"https://nomadic-app.herokuapp.com/",github:"https://github.com/Leo-GeZhao/nomadic"},  
+    {img:Tracker, title:"Tracker", desc:"personal project using Django", demo:"https://tracker.herokuapp.com/",github:"hhttps://github.com/Leo-GeZhao/Tracker"}
       
 ]
 
