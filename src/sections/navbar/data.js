c2599506f6c6e403fc022ee560f1e9eef7a3edc1
Ge Zhao <gezhao@Ges-MacBook-Pro.local>
2022-12-17 00:06:17 -0500

    fix skills section on nav

--- src/sections/navbar/data.js
+++ src/sections/navbar/data.js
@@ -1,6 +1,7 @@
 const data = [
     {link:'#',title:'Home'},
     {link:'#about',title:'About'},
+    {link:'#skills',title:'Skills'},
     {link:'#portfolio',title:'Portfolio'},
     {link:'#contact',title:'Contact'},
 ]
