112ae7e23d91d430bda2f056eb33d3ab54d2ba5b
Ge Zhao <gezhao@Ges-MacBook-Pro.local>
2022-12-16 23:58:24 -0500

    fix contact form

--- src/sections/contact/Contact.jsx
+++ src/sections/contact/Contact.jsx
@@ -1,8 +1,31 @@
+import { useRef } from 'react';
+import emailjs from 'emailjs-com'
 import {MdOutlineMail} from 'react-icons/md'
 import {AiOutlineLinkedin} from 'react-icons/ai'
 import './contact.css'
 
 const Contact = () => {
+  const form = useRef()
+
+  
+  
+  const sendEmail = (e) => {
+    e.preventDefault();
+    
+    function messageNotice(){
+      const messageBtn = e.target.querySelector('#message-btn')
+      
+      messageBtn.innerText = 'Send Successful'
+      messageBtn.style.background = 'var(--green)'
+      messageBtn.style.color = 'white'
+      messageBtn.style.transition='var(--transition)'
+    }
+
+    emailjs.sendForm(process.env.REACT_APP_YOUR_SERVICE_ID, process.env.REACT_APP_YOUR_TEMPLATE_ID, form.current, process.env.REACT_APP_YOUR_PUBLIC_KEY)
+    .then(messageNotice())
+    .then(e.target.reset())
+  };
+
   return (
     <section id="contact">
       <h2>Contact</h2>
@@ -22,11 +45,11 @@ const Contact = () => {
             rel="noopener noreferrer">Send a message</a>
           </article>
         </div>
-        <form action="">
+        <form ref={form} onSubmit={sendEmail}>
           <input type="text" name="name" placeholder='Your Full Name' required />
           <input type="email" name="email" placeholder='Your Email' required/>
           <textarea name="message" id="" rows="7" placeholder='Your Message' required></textarea>
-          <button type='submit' className='btn'>Send Message</button>
+          <button type='submit' className='btn' id="message-btn">Send Message</button>
         </form>
       </div>
     </section>
