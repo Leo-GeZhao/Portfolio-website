f7e5b7e5a8234cbb506306fa7abe163b67f94f9d
Ge Zhao <gezhao@Ges-MacBook-Pro.local>
2022-12-16 23:02:16 -0500

    add about me

--- src/sections/about/About.jsx
+++ src/sections/about/About.jsx
@@ -14,8 +14,9 @@ const About = () => {
         </div>
         <div className="about__right">
           <h3>About Me</h3>
-          <p>After 3 years of working in Accouting and Finance, I have decided to change my occupation.</p>
-          <p>Talk about why pick up Software Engineering</p>
+          <p>After 3 years of working in Accouting and Finance, I have decided on changing my career path.</p>
+          <p>The moment I discovered coding and programming, I finally found the perfect career where I could enjoy the fulfillment of solving problems and building applications.</p>
+          <p>It has been a long way, but it's my passion for software development gave me the courage to make this life changing decisions.</p>
           <a href={CV} download className='btn'>Download CV <HiDownload/></a>
         </div>
       </div>
