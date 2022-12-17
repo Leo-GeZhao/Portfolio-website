4d981689b191254a4d4d7c1e7fc8d436263354f8
Ge Zhao <gezhao@Ges-MacBook-Pro.local>
2022-12-16 21:17:21 -0500

    fix portfolio

--- src/sections/portfolio/Portfolio.jsx
+++ src/sections/portfolio/Portfolio.jsx
@@ -5,12 +5,14 @@ const Portfolio = () => {
   return (
     <section id="portfolio">
         <h2>Recent Projects</h2>
-        <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Vero, nisi consectetur labore totam ea omnis, libero asperiores facilis vel, quia dolores natus quos modi in tempora sed praesentium qui molestias.</p>
+        <p>Here is a selection of projects I've worked on. More projects are comming very soon.</p>
         <div className='container portfolio__cards'>
           {
             data.map((item,index)=>
             <Card key={index+1} className="portfolio__card">
-                      <img src={item.img} alt="" className="portfolio__card-img"/>
+                      <div className='portfolio__image'>
+                        <img src={item.img} alt="" className="portfolio__card-img"/>
+                      </div>
                       <h5>{item.title}</h5>
                       <small>{item.desc}</small>
                       <div className='btn__group'>
