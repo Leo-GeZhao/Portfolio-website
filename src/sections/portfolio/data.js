12ed57a483a41297023415f06be8a8beee57379a
Ge Zhao <gezhao@ges-macbook-pro.home>
2023-01-05 01:35:01 -0500

    add project

--- src/sections/portfolio/data.js
+++ src/sections/portfolio/data.js
@@ -1,12 +1,37 @@
-import HustleTo from '../../assets/projects/Hustle-To.png'
-import Nomadic from '../../assets/projects/Nomadic.png'
-import Tracker from '../../assets/projects/Tracker.png'
+import HustleTo from "../../assets/projects/Hustle-To.png";
+import Nomadic from "../../assets/projects/Nomadic.png";
+import Tracker from "../../assets/projects/Tracker.png";
+import FlyHead from "../../assets/projects/FlyHead.png";
 
 const data = [
-    {img:HustleTo, title:"HustleTo (In Progress)", desc:"Ecommerce website using Mern-Stack", demo:"https://hustle-to.up.railway.app/",github:"https://github.com/Leo-GeZhao/Hustle-To"},
-    {img:Nomadic, title:"Nomadic", desc:"Group project using Django", demo:"https://nomadic-app.herokuapp.com/",github:"https://github.com/Leo-GeZhao/nomadic"},  
-    {img:Tracker, title:"Tracker", desc:"personal project using Django", demo:"https://tracker.herokuapp.com/",github:"hhttps://github.com/Leo-GeZhao/Tracker"}
-      
-]
+  {
+    img: HustleTo,
+    title: "HustleTo (In Progress)",
+    desc: "Ecommerce website using Mern-Stack",
+    demo: "https://hustle-to.up.railway.app/",
+    github: "https://github.com/Leo-GeZhao/Hustle-To",
+  },
+  {
+    img: FlyHead,
+    title: "FlyHead",
+    desc: "Travel/Event Planning Website using Mern-Stack",
+    demo: "https://flyhead.herokuapp.com/",
+    github: "https://github.com/Leo-GeZhao/Flyhead",
+  },
+  {
+    img: Tracker,
+    title: "Tracker",
+    desc: "personal project using Django",
+    demo: "https://tracker.herokuapp.com/",
+    github: "hhttps://github.com/Leo-GeZhao/Tracker",
+  },
+  {
+    img: Nomadic,
+    title: "Nomadic",
+    desc: "Group project using Django",
+    demo: "https://nomadic-app.herokuapp.com/",
+    github: "https://github.com/Leo-GeZhao/nomadic",
+  },
+];
 
-export default data
+export default data;
