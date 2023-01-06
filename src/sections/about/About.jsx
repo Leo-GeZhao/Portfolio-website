02c5bce9b1da6dd73da1b8d148eb7c58a15a1903
Ge Zhao <gezhao@ges-macbook-pro.home>
2023-01-06 03:04:49 -0500

    add project

--- src/sections/about/About.jsx
+++ src/sections/about/About.jsx
@@ -1,7 +1,7 @@
-import './about.css'
-import aboutImg from '../../assets/about.jpg'
-import {HiDownload} from 'react-icons/hi'
-import CV from '../../assets/CV.pdf'
+import "./about.css";
+import aboutImg from "../../assets/about.jpg";
+import { HiDownload } from "react-icons/hi";
+import CV from "../../assets/CV.pdf";
 
 const About = () => {
   return (
@@ -14,14 +14,26 @@ const About = () => {
         </div>
         <div className="about__right">
           <h3>About Me</h3>
-          <p>After 3 years of working in Accouting and Finance, I have decided on changing my career path.</p>
-          <p>The moment I discovered coding and programming, I finally found the perfect career where I could enjoy the fulfillment of solving problems and building applications.</p>
-          <p>It has been a long way, but it's my passion for software development gave me the courage to make this life changing decisions.</p>
-          <a href={CV} download className='btn'>Download CV <HiDownload/></a>
+          <p>
+            After 3 years of working in Accouting and Finance, I have decided on
+            changing my career path.
+          </p>
+          <p>
+            The moment I discovered coding and programming, I finally found the
+            perfect career where I could enjoy the fulfillment of solving
+            problems and building applications to help myself and my surrounding
+          </p>
+          <p>
+            It has been a long way, but it's my passion for software development
+            gave me the courage to make this life changing decisions.
+          </p>
+          <a href={CV} download className="btn">
+            Download CV <HiDownload />
+          </a>
         </div>
       </div>
     </section>
-  )
-}
+  );
+};
 
-export default About
+export default About;
--- src/sections/portfolio/data.js
+++ src/sections/portfolio/data.js
@@ -2,6 +2,7 @@ import HustleTo from "../../assets/projects/Hustle-To.png";
 import Nomadic from "../../assets/projects/Nomadic.png";
 import Tracker from "../../assets/projects/Tracker.png";
 import FlyHead from "../../assets/projects/FlyHead.png";
+import SneakerVault from "../../assets/projects/Sneaker-Vault.png";
 
 const data = [
   {
@@ -19,16 +20,23 @@ const data = [
     github: "https://github.com/Leo-GeZhao/Flyhead",
   },
   {
+    img: SneakerVault,
+    title: "Sneaker-Vault",
+    desc: "Sneaker Management/Dashboard using Mern-Stack",
+    demo: "https://sneaker--vault.herokuapp.com/",
+    github: "https://github.com/Leo-GeZhao/SneakerVault",
+  },
+  {
     img: Tracker,
     title: "Tracker",
-    desc: "personal project using Django",
+    desc: "Tracker/Task-management website using Django",
     demo: "https://tracker.herokuapp.com/",
     github: "hhttps://github.com/Leo-GeZhao/Tracker",
   },
   {
     img: Nomadic,
     title: "Nomadic",
-    desc: "Group project using Django",
+    desc: "Airbnb inspired website using Django",
     demo: "https://nomadic-app.herokuapp.com/",
     github: "https://github.com/Leo-GeZhao/nomadic",
   },
