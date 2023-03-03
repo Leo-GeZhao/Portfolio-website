f388473771b7c1c912aa4e1aee48c0eeb69549e1
Ge Zhao <gezhao@ges-macbook-pro.home>
2023-03-03 12:46:53 -0500

    add new project

--- src/sections/portfolio/data.js
+++ src/sections/portfolio/data.js
@@ -3,6 +3,7 @@ import Nomadic from "../../assets/projects/Nomadic.png";
 import Tracker from "../../assets/projects/Tracker.png";
 import FlyHead from "../../assets/projects/FlyHead.png";
 import SneakerVault from "../../assets/projects/Sneaker-Vault.png";
+import DallE from "../../assets/projects/DallE.png";
 
 const data = [
   {
@@ -10,7 +11,7 @@ const data = [
     title: "HustleTo",
     subtitle: "Ecommerce website using Mern-Stack",
     desc: "As a huge sneaker enthusiast, I wanted to create a platform where I could share my passion with others and offer some of my inventory for sale. This website not only showcases my collection, but also provides a seamless shopping experience for customers with features like a shopping cart and payment integration.",
-    stack: ["NodeJS", "React", "StripAPI"],
+    stack: ["NodeJS", "React", "Stripe API"],
     demo: "https://hustle-to.herokuapp.com/",
     github: "https://github.com/Leo-GeZhao/Hustle-To",
   },
@@ -19,7 +20,7 @@ const data = [
     title: "FlyHead",
     subtitle: "Travel/Event Planning Website using Mern-Stack",
     desc: "This project is a travel planning platform that simplifies the process of finding the best restaurants, hotels, and tours for your next trip. The platform includes a built-in calendar feature to track events, making it the perfect tool for any avid traveler.",
-    stack: ["NodeJS", "React", "GoogleMapAPI", "FullCalender", "RapidAPI"],
+    stack: ["NodeJS", "React", "GoogleMap API", "FullCalender", "Rapid API"],
     demo: "https://flyhead.herokuapp.com/",
     github: "https://github.com/Leo-GeZhao/Flyhead",
   },
@@ -28,9 +29,18 @@ const data = [
     title: "Sneaker-Vault",
     subtitle: "Sneaker Management/Dashboard using Mern-Stack",
     desc: "This project is a sneaker management dashboard that enables me to efficiently manage my entire sneaker inventory. I've integrated an unofficial StockX API to track real-time ask/bid prices and keep a close eye on the market value of each sneaker. Using this dashboard, I can easily monitor my inventory and make informed decisions about which sneakers to sell and when.",
-    stack: ["NodeJS", "React", "StockXAPI"],
+    stack: ["NodeJS", "React", "StockX API"],
     demo: "https://sneaker--vault.herokuapp.com/",
     github: "https://github.com/Leo-GeZhao/SneakerVault",
+  },
+  {
+    img: DallE,
+    title: "OpenAI Dall-E 2.0 Clone",
+    subtitle: "Creating Art with AI: An OpenAI DALL-E Clone",
+    desc: "To create the project, I designed and implemented a React-based front-end interface that allows users to input text and view the corresponding generated images. On the back-end, I integrated the OpenAI API and built a custom server using Node.js and Express.js. This allowed my application to send natural language input to the OpenAI API and receive corresponding images that were rendered in real-time on the front-end.",
+    stack: ["NodeJS", "React", "OpenAI API"],
+    demo: "https://dall-e-2-clone-0nmw.onrender.com/",
+    github: "https://github.com/Leo-GeZhao/Dall-E-Clone",
   },
   {
     img: Tracker,
